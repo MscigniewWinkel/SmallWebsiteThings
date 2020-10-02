f7508ac3e6aa57b8f623061d59511c682e586acc
MscigniewWinkel <50490989+MscigniewWinkel@users.noreply.github.com>
2020-10-02 21:31:13 +0200

    Add files via upload

--- hoverTileGame.js
+++ hoverTileGame.js
@@ -1,105 +1,112 @@
 let targetBoard = []
-
 let playerBoard = []
 
-tBoardExists = false
-
 let tilesStyles = ['squareWhite tile', 'squareBlack tile']
+let colorPointers = ['w', 'b']
 
-function createTBoard() {
+function swapTiles(){
+
+    temp0 = tilesStyles[0]
+    temp1 = tilesStyles[1]
+    tilesStyles = [temp1, temp0]
+
+}
+
+function swapColorPointers(){
     
+    colorTemp0 = colorPointers[0]
+    colorTemp1 = colorPointers[1]
+    colorPointers = [colorTemp1, colorTemp0]
+
+}
+
+function checkGameStatus(){
+
+    checkForVictory = true
+
+        for (var i = 0; i < playerBoard.length; i++) {
+
+            if (playerBoard[i] != targetBoard[i]) {
+
+                checkForVictory = false
+                break
+
+            }
+        }
+
+        if (checkForVictory) {
+            
+            $(".tile").off()
+            $("#description > span").text("Gratulacje! Wygrales!").css({"background": "#3ac928"})
+            $("#site").css("background", "radial-gradient(circle, rgba(140,255,54,1) 0%, rgba(126,255,124,1) 100%)")
+
+        }
+
+}
+
+function createTilesBoard() {
+
     for (x = 0; x < 64; x++) {
-        if (tBoardExists === true) {break}
-        let el = document.createElement('div')
-        el.innerText=x
-        el.className = 'squareBlack tile'
 
+        $("#board").append(`<div class="${tilesStyles[1]}">${x}</div>`)
         playerBoard[x] = 'b'
-        
-        document.getElementById('board').appendChild(el)        
+
     }
-
-    tBoardExists = true    
-
 }
 
-function createTargetBoard(){
+function createTargetBoard() {    
 
-    colorPointers = ['w', 'b']    
+    for (x = 0; x < 64; x++) {
 
-    for (x = 0; x < 64; x++) {        
-                
-        if (x%2 === 0) {
-            let el = document.createElement('div')
-            el.innerText=x
-            el.className = tilesStyles[0]
+        if (x % 2 === 0) {     
+
+            $("#targetBoard").append(`<div class="${tilesStyles[0]}">${x}</div>`)
             targetBoard[x] = colorPointers[0]
-        
-            document.getElementById('targetBoard').appendChild(el)
 
-        } else if (x%2 === 1) {
+        } else if (x % 2 === 1) {
 
-            let el = document.createElement('div')
-            el.innerText=x
-            el.className = tilesStyles[1]
+            $("#targetBoard").append(`<div class="${tilesStyles[1]}">${x}</div>`)
             targetBoard[x] = colorPointers[1]
-        
-            document.getElementById('targetBoard').appendChild(el)
+
         }
 
-        if ((x+1)%8 === 0){
-            temp0 = tilesStyles[0]
-            temp1 = tilesStyles[1]
-            tilesStyles = [temp1, temp0]
-
-            colorTemp0 = colorPointers[0]
-            colorTemp1 = colorPointers[1]
-            colorPointers = [colorTemp1, colorTemp0]
+        if ((x + 1) % 8 === 0) {
+            
+            swapTiles()
+            swapColorPointers()
+            
         }
-        
-        console.log(targetBoard[x])
-                    
     }
 }
 
-function play(){
-    $(".tile").mouseover(function(){
 
-        if ($(this).hasClass("squareBlack")){
+
+function play() {
+    
+    $(".tile").mouseover(function () {
+
+        if ($(this).hasClass("squareBlack")) {
+
             $(this).toggleClass("squareBlack", false)
             $(this).toggleClass("squareWhite", true)
 
-            playerBoard[this.innerText] = 'w'
-            console.log(playerBoard[this.innerText])
-         } 
-        else if ($(this).hasClass("squareWhite")){
+            playerBoard[this.innerText] = 'w'            
+
+        } else if ($(this).hasClass("squareWhite")) {
+
             $(this).toggleClass("squareWhite", false)
             $(this).toggleClass("squareBlack", true)
 
-            playerBoard[this.innerText] = 'b'
-            console.log(playerBoard[this.innerText])
+            playerBoard[this.innerText] = 'b'            
+
         }
 
-        checkForVictory = true
-
-        for (var i = 0; i<playerBoard.length; i++){
-            if (playerBoard[i] != targetBoard[i]){checkForVictory = false}
-        }
-
-        if (checkForVictory){
-            $("#description").text("Gratulacje! Wygrales!")
-            $(".tile").off()
-            $("#description").css({"background-color":"limegreen", 
-                "padding":"25px",
-                "border-radius":"10px 10px 10px 10px"
-            })
-            $("#site").css("background-color", "rgb(180, 255, 168)")
-        }
-    }) 
+        checkGameStatus()
+    })
 }
 
-function theGame(){
-    createTBoard()
+function theGame() {
+    createTilesBoard()
     play()
     createTargetBoard()
 }
